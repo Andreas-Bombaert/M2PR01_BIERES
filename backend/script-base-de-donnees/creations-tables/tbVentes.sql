ab2ab7fe88fd36c17ca2a98a1992ab3065795709
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-04-23 16:24:23 +0200

    Update tbVentes.sql

--- backend/script-base-de-donnees/creations-tables/tbVentes.sql
+++ backend/script-base-de-donnees/creations-tables/tbVentes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "tbVentes" (
     "commId" INTEGER NOT NULL,
     "ligneNo" INTEGER NOT NULL,
-    "biereID" decimal(6,2) NOT NULL,
+    "biereId" INTEGER NOT NULL,
     "prodQuant" INTEGER NOT NULL,
     CONSTRAINT "fk__tbVentes__tbHistoriques" FOREIGN KEY (commId) REFERENCES tbHistoriques (commId)
 );
