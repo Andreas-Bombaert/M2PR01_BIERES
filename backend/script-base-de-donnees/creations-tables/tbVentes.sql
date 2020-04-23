32b1d22420dde2a5cdfcf06012d28d5ada65476f
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-04-23 16:17:27 +0200

    Create tbVentes.sql

---
+++ backend/script-base-de-donnees/creations-tables/tbVentes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "tbVentes" (
+    "commID" char(4) NOT NULL,
+    "ligneNo" INTEGER NOT NULL,
+    "biereID" decimal(6,2) NOT NULL,
+    "prodQuant" INTEGER NOT NULL,
+    CONSTRAINT "pk__tbVentes" PRIMARY KEY ( "commID" ASC ),
+    CONSTRAINT "fk__tbVentes__tbHistorique" FOREIGN KEY (commID) REFERENCES tbHistoriques (commID)
+);
