f63bdc9537af335e389f1b8c97dbe01aab6a14ce
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-04-23 16:35:12 +0200

    Update tbBrasseurs.sql

--- backend/script-base-de-donnees/creations-tables/tbBrasseurs.sql
+++ backend/script-base-de-donnees/creations-tables/tbBrasseurs.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tbBrasseurs(
-    brasseurId INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    brasseurId char(3) NOT NULL,
     brasseurNom VARCHAR(20) NOT NULL,
     brasseurVille1 VARCHAR(50) NOT NULL,
     brasseurVille2 VARCHAR(50) NULL,
