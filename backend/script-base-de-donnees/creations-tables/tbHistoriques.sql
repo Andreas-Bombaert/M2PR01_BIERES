6e9b030d738433b345ba2536bac6c275e7ee7010
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-04-23 16:22:57 +0200

    Update tbHistoriques.sql

--- backend/script-base-de-donnees/creations-tables/tbHistoriques.sql
+++ backend/script-base-de-donnees/creations-tables/tbHistoriques.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "tbHistoriques" (
-    "commId" char(4) NOT NULL DEFAULT AUTOINCREMENT,
-    "clientId" char(5) NOT NULL,
+    "commId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
+    "clientId" INTEGER NOT NULL,
     "prixTot" decimal(6,2) NOT NULL,
     "quantTot" INTEGER NOT NULL,
     "date" "datetime" NOT NULL DEFAULT getDate(),
