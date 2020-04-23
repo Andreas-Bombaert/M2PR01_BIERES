3a66e08907a206ec2529ff2ca6a66bf8b3ac3554
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-04-23 20:24:20 +0200

    Update tbBieres.sql

--- backend/script-base-de-donnees/creations-tables/tbBieres.sql
+++ backend/script-base-de-donnees/creations-tables/tbBieres.sql
@@ -5,7 +5,7 @@ biereCouleur VARCHAR(15) NOT NULL,
 biereAlcool DECIMAL NULL,
 biereVolume NUMERIC NOT NULL, 
 bierePrix DECIMAL NOT NULL,
-brasseurId char(3) NOT NULL,
+brasseurId char(4) NOT NULL,
 CONSTRAINT pk_tbBieres PRIMARY KEY (biereId),
 CONSTRAINT fk_tbBieres FOREIGN KEY ( brasseurId ) REFERENCES tbBrasseurs ( brasseurId )
 on update cascade
