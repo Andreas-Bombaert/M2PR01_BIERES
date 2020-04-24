6deb5608bd698cf363ccb2c235499a1e72e48a6c
Andréas Bombaert <64082850+Andreas-Bombaert@users.noreply.github.com>
2020-04-24 14:16:53 +0200

    + webservices de base

--- backend/scripts-serveur-web/procedures-base.sql
+++ backend/scripts-serveur-web/procedures-base.sql
@@ -4,14 +4,15 @@ deterministic
 BEGIN
 declare dbPath long varchar; // chemin de la db
 declare dbName long varchar; // nom de la db
---
+
 set dbPath = (SELECT db_property ('file'));
 set dbName = (SELECT db_property('name')) + '.db';
 set dbPath = left(dbPath, length(dbPath)-length(dbName));
---
+
 return dbPath; // renvoyer path
 END
 
+---
 
 CREATE PROCEDURE "dba"."http_getPage"(in url char(255))
 RESULT(html long varchar)
@@ -20,7 +21,7 @@ BEGIN
     select xp_read_file(dba.getPath() ||url|| '.html');
 END
 
-
+---
 
 CREATE PROCEDURE "dba"."http_getCSS"(in url char(255))
 RESULT(css long varchar)
@@ -29,7 +30,7 @@ call sa_set_http_header( 'Content-Type', 'text/css'); // header http
 select xp_read_file(dba.getPath() || 'CSS\' || url); // renvoyer css
 END
 
-
+---
 
 CREATE PROCEDURE "dba"."http_getJS"(in url char(255))
 RESULT(js long varchar)
@@ -38,6 +39,7 @@ call sa_set_http_header( 'Content-Type', 'text/javascript'); // header
 select xp_read_file(dba.getPath() || 'JS\' || url); // renvoyer js
 END
 
+---
 
 CREATE PROCEDURE "dba"."http_getIMG"( in url char(255) )
 result( img long binary ) dynamic result sets 1
@@ -51,7 +53,7 @@ begin
 --
 end
 
-
+---
 
 CREATE PROCEDURE "dba"."http_getVideo"(in url char(255))
 result(html long varchar)
@@ -61,3 +63,10 @@ BEGIN
 END
 
 --------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE SERVICE "root" TYPE 'RAW' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call http_getPage('index');
+CREATE SERVICE "css" TYPE 'RAW' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.http_getCSS(:url);
+CREATE SERVICE "js" TYPE 'RAW' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.http_getJS(:url);
+CREATE SERVICE "img" TYPE 'RAW' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.http_getIMG(:url);
+CREATE SERVICE "video" TYPE 'RAW' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.http_getVideo(:url);
+
