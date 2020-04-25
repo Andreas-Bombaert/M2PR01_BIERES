5623f6fcb1b310e01777c5a85d30130df9b2a72e
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-04-25 12:01:58 +0200

    Update creation-tables.sql

--- backend/script-base-de-donnees/creation-tables.sql
+++ backend/script-base-de-donnees/creation-tables.sql
@@ -30,7 +30,7 @@ CREATE TABLE tbBieres(
     biereId CHAR(3) NOT NULL,                   /* b01, b02, b03 ... */ 
     biereNom VARCHAR(30) NOT NULL,
     biereCouleur VARCHAR(15) NOT NULL, 
-    biereAlcool DECIMAL(4,2) NULL,
+    biereAlcool DECIMAL(3,1) NULL,
     biereVolume DECIMAL(4,2) NOT NULL, 
     bierePrix DECIMAL(4,2) NOT NULL,
     brasseurId char(4) NOT NULL,
