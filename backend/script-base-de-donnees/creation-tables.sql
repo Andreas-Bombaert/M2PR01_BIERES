c7e70760e967a124a52c35e6f3a218bb0cb5fd00
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-04-25 11:57:26 +0200

    Update creation-tables.sql

--- backend/script-base-de-donnees/creation-tables.sql
+++ backend/script-base-de-donnees/creation-tables.sql
@@ -30,9 +30,9 @@ CREATE TABLE tbBieres(
     biereId CHAR(3) NOT NULL,                   /* b01, b02, b03 ... */ 
     biereNom VARCHAR(30) NOT NULL,
     biereCouleur VARCHAR(15) NOT NULL, 
-    biereAlcool DECIMAL(6,2) NULL,
-    biereVolume DECIMAL(6,2) NOT NULL, 
-    bierePrix DECIMAL(6,2) NOT NULL,
+    biereAlcool DECIMAL(4,2) NULL,
+    biereVolume DECIMAL(4,2) NOT NULL, 
+    bierePrix DECIMAL(4,2) NOT NULL,
     brasseurId char(4) NOT NULL,
     CONSTRAINT pk__tbBieres PRIMARY KEY (biereId),
     CONSTRAINT fk__tbBieres__tbBrasseurs FOREIGN KEY ( brasseurId ) REFERENCES tbBrasseurs ( brasseurId )
