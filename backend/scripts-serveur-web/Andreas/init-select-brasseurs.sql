8a3a935c21b90c8455d9c7dcb6f3a6419052943f
Andréas Bombaert <64082850+Andreas-Bombaert@users.noreply.github.com>
2020-04-25 12:14:45 +0200

    Create init-select-brasseurs.sql

---
+++ backend/scripts-serveur-web/Andreas/init-select-brasseurs.sql
@@ -0,0 +1,14 @@
+
+/*
+Creation de la procédure qui sélectionne les id et les noms des brasseurs
+*/
+CREATE PROCEDURE "dba"."init_brasseurs"()
+BEGIN
+    Call sa_set_http_header('Access-Control-Allow-Origin','*');
+    call sa_set_http_header('Content-Type','application/json');
+    select brasseurId, brasseurNom from dba.tbBrasseurs
+    order by brasseurNom ASC;
+END
+
+//Creation du service
+CREATE SERVICE "initBrasseurs" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.init_brasseurs();
