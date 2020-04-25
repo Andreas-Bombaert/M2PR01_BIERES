c3bf99e56053aebde6b6b173f313af4f17a6af14
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-04-25 21:15:48 +0200

    Add files via upload

---
+++ backend/scripts-serveur-web/Mathis/webServices/webServicesCatalogue.sql
@@ -0,0 +1,5 @@
+CREATE SERVICE "allBr" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call DBA.proc_catalogueAllBr(:couleur,:pMin,:pMax,:dMin,:dMax);
+CREATE SERVICE "allBrAllColors" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.proc_catalogueAllBrAllColors(:pMin,:pMax,:dMin,:dMax);
+CREATE SERVICE allColorsTYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.proc_catalogueAllColors(:brasserie,:pMin,:pMax,:dMin,:dMax);
+CREATE SERVICE "biereTrie" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.proc_catalogueTrie(:couleur,:brasserie,:pMin,:pMax,:dMin,:dMax);
+CREATE SERVICE "initBieres" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.proc_catalogueEntier();
