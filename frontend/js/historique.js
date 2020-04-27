811db289df5a6b647c2350fb5292ddd15a4dab14
Cédric De Dryver <DeDryverC@users.noreply.github.com>
2020-04-27 16:21:28 +0200

    getHistory

--- frontend/js/historique.js
+++ frontend/js/historique.js
@@ -1,28 +1,39 @@
 
 function connexionHistorique(form){
     let clientId ="";
+    let mayStop = false;
     let xhr =  new XMLHttpRequest();
-    xhr.open('get','connexion?mail='+form.mail.value+'&mdp='+form.mdp.value, true);
-    xhr.onload = function(){
-        let jsoned = JSON.parse(xhr.responseText);
-        console.log(jsoned);
-        if(jsoned == ""){
+    xhr.open('get','connexion?mail='+form.mail.value+'&mdp='+form.mdp.value,false);
+    xhr.onload = function xhrjsoned1(){
+        let jsonClient = JSON.parse(xhr.responseText);
+        console.log(jsonClient);
+        if(Object.keys(jsonClient).length === 0){
+
             document.querySelector('#recuHistorique').innerHTML="<p> Mauvais identifiant ou mauvais mot de passe</p>";
+            mayStop = true;
         }
         else{
-            clientId = jsoned[0].clientId;
+            clientId = String(jsonClient[0].clientId);
         }
 
     };
     xhr.send();
+    if(mayStop == true){
+        return false;
+    }
 
     let xhr2 = new XMLHttpRequest();
-    console.log(clientId);
-    xhr2.open('get','getHistory?code='+clientId, true);
-    xhr2.onload = function(){
-        let jsoned2 = JSON.parse(xhr2.responseText);
-        console.log(jsoned2);
+    xhr2.open('get','getHistory?code='+clientId,false);
+    xhr2.onload = function xhrjsoned2(){
+        let tabRetour="<table>"+"<thead><th>Id de la commande</th><th>Prix Total</th><th>Quantité Totale</th> </thead>";
+
+        let jsonHistorique = JSON.parse(xhr2.responseText);
+        for(let i in jsonHistorique){
+            tabRetour +="<tr><td>"+jsonHistorique[i].commId+"</td><td>"+jsonHistorique[i].prixTot+"€ </td><td>"+jsonHistorique[i].quantTot+"</td></tr>";
+        }
+        document.getElementById("recuHistorique").innerHTML = tabRetour;
     };
+
     xhr2.send();
 
     return false;
