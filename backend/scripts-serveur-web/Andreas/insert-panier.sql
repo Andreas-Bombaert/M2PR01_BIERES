24b5428886aa316643c44be67386a96260c97079
Andréas Bombaert <64082850+Andreas-Bombaert@users.noreply.github.com>
2020-04-27 16:56:13 +0200

    Update insert-panier.sql

--- backend/scripts-serveur-web/Andreas/insert-panier.sql
+++ backend/scripts-serveur-web/Andreas/insert-panier.sql
@@ -1,6 +1,11 @@
+//modifier en fonction de comment on appelera la table panier
+
 CREATE PROCEDURE "dba"."proc_insert_panier"(in bId char(3), in qtt integer)
 BEGIN 
     INSERT INTO dba.panier (biereId,quantite)
     VALUES 
     (bId,qtt);
 END
+
+
+CREATE SERVICE "insertPanier" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insert_panier(:bId,:qtt);
