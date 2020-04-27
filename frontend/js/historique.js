ac2bbcc1572a4884714269577ba5963b078d1edb
Cédric De Dryver <DeDryverC@users.noreply.github.com>
2020-04-27 14:12:45 +0200

    Update Historique

--- frontend/js/historique.js
+++ frontend/js/historique.js
@@ -1,9 +1,10 @@
 
 function connexionHistorique(form){
-/*    let xhr =  new XMLHttpRequest();
+    let xhr =  new XMLHttpRequest();
     xhr.open('get','connexion?mail='+form.mail.value+'&mdp='+form.mdp.value);
     xhr.onload = function(){
         let jsoned = JSON.parse(xhr.responseText);
+        console.log(jsoned);
         if(jsoned == ""){
             document.querySelector('#recuHistorique').innerHTML="<p> Mauvais identifiant ou mauvais mot de passe</p>";
         }
@@ -12,7 +13,5 @@ function connexionHistorique(form){
         }
     }
     xhr.send();
-*/
-console.log("baguette");
     return false;
 }
