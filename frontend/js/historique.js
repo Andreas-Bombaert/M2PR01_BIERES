31149699cc6ddabf1282eaecf515176843e9f16c
Cédric De Dryver <DeDryverC@users.noreply.github.com>
2020-04-27 15:26:24 +0200

    Update historique

--- frontend/js/historique.js
+++ frontend/js/historique.js
@@ -1,7 +1,8 @@
 
 function connexionHistorique(form){
+    let clientId ="";
     let xhr =  new XMLHttpRequest();
-    xhr.open('get','connexion?mail='+form.mail.value+'&mdp='+form.mdp.value);
+    xhr.open('get','connexion?mail='+form.mail.value+'&mdp='+form.mdp.value, true);
     xhr.onload = function(){
         let jsoned = JSON.parse(xhr.responseText);
         console.log(jsoned);
@@ -9,9 +10,20 @@ function connexionHistorique(form){
             document.querySelector('#recuHistorique').innerHTML="<p> Mauvais identifiant ou mauvais mot de passe</p>";
         }
         else{
-            document.querySelector("#recuHistorique").innerHTML = jsoned;
+            clientId = jsoned[0].clientId;
         }
-    }
+
+    };
     xhr.send();
+
+    let xhr2 = new XMLHttpRequest();
+    console.log(clientId);
+    xhr2.open('get','getHistory?code='+clientId, true);
+    xhr2.onload = function(){
+        let jsoned2 = JSON.parse(xhr2.responseText);
+        console.log(jsoned2);
+    };
+    xhr2.send();
+
     return false;
 }
