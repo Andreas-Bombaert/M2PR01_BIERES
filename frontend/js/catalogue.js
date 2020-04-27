5eb16b651d42c815f79782ef6de49fe9e3a24291
Andréas Bombaert <64082850+Andreas-Bombaert@users.noreply.github.com>
2020-04-27 14:13:28 +0200

    - doublon de initBrasseurs

--- frontend/js/catalogue.js
+++ frontend/js/catalogue.js
@@ -134,27 +134,6 @@ function initBrasseurs(){
     xhr.send();
 }
 
-/**
- * Date: 25-04-2020
- * @author Andréas Bombaert
- *
- * fonction qui récupère les noms des brasseurs et leurs Id afin de les injecter
- * dans le select du formulaire de commandes
- */
-
-function initBrasseurs(){
-    let xhr= new XMLHttpRequest();
-    xhr.open('get','initBrasseurs');
-    xhr.onload=function(){
-        let parsed=JSON.parse(xhr.responseText);
-        let str="<option value='allBr'>Toutes</option>";
-        for(let x of parsed){
-            str+="<option value='"+x.brasseurId+"'>"+x.brasseurNom+"</option>";
-        }
-        document.getElementById("brasserieBiere").innerHTML=str;
-    };
-    xhr.send();
-}
 
 //ajoute la bière dans le panier de commande en fonction des quantités demandées
 function addBr(biere,id,brasserie,volume,qtt,prix){
