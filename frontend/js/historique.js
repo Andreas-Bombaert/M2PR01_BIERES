b1246f1afa24903b461fb20146f7b456035ed724
Cédric De Dryver <DeDryverC@users.noreply.github.com>
2020-04-27 18:43:52 +0200

    Edit History
    ça ne marche pas mais un jour ça va marcher

--- frontend/js/historique.js
+++ frontend/js/historique.js
@@ -1,11 +1,18 @@
 
 function connexionHistorique(form){
+
+    /*<--- VARIABLES --->*/
+
     let clientId ="";
     let mayStop = false;
+    let jsonClient,jsonHistorique,jsonVentes;
+    let strRetour ="";
+
+
     let xhr =  new XMLHttpRequest();
     xhr.open('get','connexion?mail='+form.mail.value+'&mdp='+form.mdp.value,false);
     xhr.onload = function xhrjsoned1(){
-        let jsonClient = JSON.parse(xhr.responseText);
+        jsonClient = JSON.parse(xhr.responseText);
         console.log(jsonClient);
         if(Object.keys(jsonClient).length === 0){
 
@@ -18,23 +25,41 @@ function connexionHistorique(form){
 
     };
     xhr.send();
-    if(mayStop == true){
+
+    if(mayStop === true){
         return false;
     }
 
-    let xhr2 = new XMLHttpRequest();
-    xhr2.open('get','getHistory?code='+clientId,false);
-    xhr2.onload = function xhrjsoned2(){
-        let tabRetour="<table>"+"<thead><th>Id de la commande</th><th>Prix Total</th><th>Quantité Totale</th> </thead>";
-
-        let jsonHistorique = JSON.parse(xhr2.responseText);
-        for(let i in jsonHistorique){
-            tabRetour +="<tr><td>"+jsonHistorique[i].commId+"</td><td>"+jsonHistorique[i].prixTot+"€ </td><td>"+jsonHistorique[i].quantTot+"</td></tr>";
-        }
-        document.getElementById("recuHistorique").innerHTML = tabRetour;
+    xhr.open('get','getHistory?code='+clientId,true);
+    xhr.onload= function historique(e){
+        let jsoned = xhr.responseText;
+        jsonHistorique = JSON.parse(jsoned);
+        console.log(jsonHistorique);
+        e = jsonHistorique;
+        return jsonHistorique;
     };
+    console.log(jsonHistorique); /** TODO: SQL GROUP BY **/
+    xhr.send();
+    console.log(jsonHistorique+'BBBBBBB');
+    /*
+        strRetour +="<table>"+"<thead><th>Id de la commande</th><th>Objet commandé</th><th>Quantité</th><th>Prix Total</th><th>Quantité Totale</th> </thead>";
 
-    xhr2.send();
+        let xhr3 = new XMLHttpRequest();
 
+        for(let i in jsonHistorique) {
+
+            strRetour += "<tr><td></td><td></td><td>" + jsonHistorique[i].commId + "</td><td>" + jsonHistorique[i].prixTot + "€ </td><td>" + jsonHistorique[i].quantTot + "</td></tr>";
+
+            xhr3.open('get','getVentes?id='+jsonHistorique[i].commId, true);
+
+            xhr3.onload= function(){
+                jsonVentes = JSON.parse(xhr3.responseText);
+                for(let j in jsonVentes){
+                    strRetour +="<tr><td></td><td></td><td>jsonVentes[j].biereId</td><td>jsonVentes[j].prodQuant</td><td></td><td></td></tr>";
+                }
+            xhr3.send();
+            }
+        }
+        gid("recuHistorique").innerHTML = strRetour;*/
     return false;
 }
