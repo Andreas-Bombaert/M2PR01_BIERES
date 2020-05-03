c557967d8b09e595f63f53f79a66f396c22c79e3
Eliott-Lepage <64136394+Eliott-Lepage@users.noreply.github.com>
2020-05-03 23:03:16 +0200

    Update insert-panier.sql

--- backend/scripts-serveur-web/Andreas/insert-panier.sql
+++ backend/scripts-serveur-web/Andreas/insert-panier.sql
@@ -2,7 +2,7 @@
 
 CREATE PROCEDURE "dba"."proc_insert_panier"(in bId char(3), in qtt integer)
 BEGIN 
-    INSERT INTO dba.panier (biereId,quantite)
+    INSERT INTO dba.tbPanier (biereId,quantite)
     VALUES 
     (bId,qtt);
 END
