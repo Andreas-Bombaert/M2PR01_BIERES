fa89f28bc0ae6d52c4ea62d5efeda17293a82874
Eliott-Lepage <64136394+Eliott-Lepage@users.noreply.github.com>
2020-05-04 14:55:50 +0200

    Update tbPanier.sql

--- backend/script-base-de-donnees/creations-tables/tbPanier.sql
+++ backend/script-base-de-donnees/creations-tables/tbPanier.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "DBA"."tbPanier" (
-	"biereId" CHAR(3) NOT NULL,
-	"quantite" INTEGER NOT NULL,
-	CONSTRAINT "pk__tbPanier" PRIMARY KEY ( "biereId" ASC )
+    "biereId" CHAR(3) NOT NULL,             /* b01, b02, b03 ... */ 
+    "quantite" INTEGER NOT NULL,
+    CONSTRAINT "pk__tbPanier" PRIMARY KEY ( "biereId" ),
+    CONSTRAINT fk__tbPanier__tbBieres FOREIGN KEY ( biereId ) REFERENCES tbPanier ( biereId )
 );
