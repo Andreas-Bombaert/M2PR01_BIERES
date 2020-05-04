07395a9428327b20f8709da0bd9e0294ad9d2591
Andréas Bombaert <64082850+Andreas-Bombaert@users.noreply.github.com>
2020-05-04 14:57:03 +0200

    Update creation-tables.sql

--- backend/script-base-de-donnees/creation-tables.sql
+++ backend/script-base-de-donnees/creation-tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE "DBA"."tbPanier" (
     "biereId" CHAR(3) NOT NULL,             /* b01, b02, b03 ... */ 
     "quantite" INTEGER NOT NULL,
     CONSTRAINT "pk__tbPanier" PRIMARY KEY ( "biereId" ),
-    CONSTRAINT fk__tbPanier__tbBieres FOREIGN KEY ( biereId ) REFERENCES tbPanier ( biereId )
+    CONSTRAINT fk__tbPanier__tbBieres FOREIGN KEY ( biereId ) REFERENCES tbBieres ( biereId )
 );
 
 
