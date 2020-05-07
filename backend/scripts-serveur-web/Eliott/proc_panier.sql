9451613ea8035de7044ed481bed08919f493c583
Eliott-Lepage <64136394+Eliott-Lepage@users.noreply.github.com>
2020-05-07 23:03:15 +0200

    Update proc_panier.sql

--- backend/scripts-serveur-web/Eliott/proc_panier.sql
+++ backend/scripts-serveur-web/Eliott/proc_panier.sql
@@ -6,17 +6,17 @@ avec les informations les concernant (biereId, biereNom, biereAlcool et quantit√
 
 */
 
-CREATE PROCEDURE "DBA"."proc_panier"()
-RESULT (idBiere char(3), nomBiere char(30), alcoolBiere INTEGER, volumeBiere INTEGER, quantitBiere INTEGER)
+ALTER PROCEDURE "DBA"."proc_panier"()
+RESULT (idBiere char(3), nomBiere char(30), alcoolBiere INTEGER, volumeBiere INTEGER, prixBiere INTEGER, quantitBiere INTEGER, prixTotal INTEGER)
 BEGIN 
     CALL sa_set_http_header('Access-Control-Allow-Origin','*');
     CALL sa_set_http_header('Content-Type','application/json');
-    SELECT panier.biereId, biereNom, biereAlcool, biereVolume, sum(quantite) as quantite
-    FROM dba.tbBieres as biere JOIN dba.tbPanier as panier
-         ON biere.biereId = panier.biereId
-    GROUP BY panier.biereId, biere.biereNom, biere.biereAlcool, biere.biereVolume
+    SELECT panier.biereId, biereNom, biereAlcool, biereVolume, bierePrix, sum(quantite) as Quantite, quantite * bierePrix as Total
+        FROM dba.tbBieres as biere JOIN dba.tbPanier as panier
+           ON biere.biereId = panier.biereId
+    GROUP BY panier.biereId, biere.biereNom, biere.biereAlcool, biere.biereVolume, biere.bierePrix
     ORDER BY biere.biereNom;
-END;
+END
 
 
 /* Creation du service : pour recevoirt le code JSON ( 'get' )*/
