054714f32da0d4de194bb03f18491dfa062343d1
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-05-08 15:31:43 +0200

    Update getBieres.sql

--- backend/scripts-serveur-web/Cedric/getBieres.sql
+++ backend/scripts-serveur-web/Cedric/getBieres.sql
@@ -1,10 +1,10 @@
 
 /*
-auth : Cédric
+auth : Cédric De Dryver
 
 Procedure et service pour l'onglet Historique.
 
-Retourne 
+Retourne la liste complète des biereId et les les biereNom
 */
 
 
@@ -15,5 +15,7 @@ BEGIN
     call sa_set_http_header('Content-Type','application/json');
     Select biereId, biereNom from tbBieres
 end
+
+/* service pour la procedure */
 
 CREATE SERVICE "getBieres" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS CALL dba.getBiereNom();
