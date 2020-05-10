6d027d216f378aacb55731af33783996ca3ac738
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 15:53:22 +0200

    Delete tbPanier.sql

--- backend/script-base-de-donnees/creations-tables/tbPanier.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "DBA"."tbPanier" (
-    "biereId" CHAR(3) NOT NULL,             /* b01, b02, b03 ... */ 
-    "quantite" INTEGER NOT NULL,
-    CONSTRAINT "pk__tbPanier" PRIMARY KEY ( "biereId" ),
-    CONSTRAINT fk__tbPanier__tbBieres FOREIGN KEY ( biereId ) REFERENCES tbPanier ( biereId )
-);
