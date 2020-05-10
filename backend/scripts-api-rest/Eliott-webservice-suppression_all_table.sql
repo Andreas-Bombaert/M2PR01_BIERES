10f4e23af23877b6b6fa5dc54e65265e93c7c7f4
Andréas Bombaert <64082850+Andreas-Bombaert@users.noreply.github.com>
2020-05-10 15:41:57 +0200

    Update Eliott-webservice-suppression_all_table.sql

--- backend/scripts-api-rest/Eliott-webservice-suppression_all_table.sql
+++ backend/scripts-api-rest/Eliott-webservice-suppression_all_table.sql
@@ -4,15 +4,16 @@ Création de de la procédure qui supprime la totalité de la table tbPanier con
 
 */
 
-CREATE PROCEDURE "DBA"."proc_suppression_all_table"()
+ALTER PROCEDURE "dba"."proc_suppression_all_table"(in clId char(5))
 BEGIN 
-    DELETE FROM tbPanier;
-END;
+    delete from tbPanier
+    where clientId=clId;
+END
 
 
 /* Création du webservice lié à la procédure */
 
-CREATE SERVICE "suppression_all" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call DBA.proc_suppression_all_table();
+CREATE SERVICE "suppression_all" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call DBA.proc_suppression_all_table(:clId);
 
 
 
