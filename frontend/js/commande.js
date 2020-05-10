bf3e6bda0ff929ebd6ad98f29f71b4619a6f057b
Andréas Bombaert <64082850+Andreas-Bombaert@users.noreply.github.com>
2020-05-10 16:04:32 +0200

    Update commande.js

--- frontend/js/commande.js
+++ frontend/js/commande.js
@@ -50,7 +50,7 @@ function panier(clientId) {
             }
 
             str += "<tr class='vide'><td></td><td></td><td></td><td></td><td></td><td></td></tr>";
-            str += "<tr class='vide'><td><input type='submit' id='toutSupprimer' value='Tout supprimer' onclick='toutSupprimer(" + '"' + clientId + '"' + ");'></td>";  // Création du bouton de suppression (toute la table)
+            str += "<tr class='vide'><td><input type='submit' id='toutSupprimer' value='Tout supprimer' onclick='toutSupprimer(" + '"' + clientId + '"' + ");'></td><input type='submit' id='commander' value='Commander' onclick='commander("+'"'+clientId+'"'+");'></td>";  // Création du bouton de suppression (toute la table)
 
             str += "<td></td><td></td><td></td><td class='TitreSomme'>" + "Total :" + "</td><td id='somme'>" + montantTotal.toFixed(2) + " €" + "</td></tr>";
             str += "</tbody>";
