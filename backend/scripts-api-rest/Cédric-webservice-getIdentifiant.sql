85f7e7dc4cd74f5e5ce59cc56fec126288a55803
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-05-10 17:35:33 +0200

    Update Cédric-webservice-getIdentifiant.sql

--- backend/scripts-api-rest/Cédric-webservice-getIdentifiant.sql
+++ backend/scripts-api-rest/Cédric-webservice-getIdentifiant.sql
@@ -5,13 +5,12 @@ Procedure et service pour avoir les identifiants (et verifier qu'ils sont bon).
 Retourne le clientId
 */
 
-
-CREATE PROCEDURE "DBA"."proc_getIdentifiant" (IN mail VARCHAR(50),IN motDePasse VARCHAR(20))
-RESULT(identifiant VARCHAR(20), mdp VARCHAR(20), clientId CHAR(5))
+CREATE PROCEDURE "DBA"."getIdentifiant" (IN mail VARCHAR(50),IN motDePasse VARCHAR(20))
+RESULT(identifiant VARCHAR(20), mdp VARCHAR(20), clientId CHAR(5), clientPrenom VARCHAR(20))
 BEGIN
     call sa_set_http_header('Access-Control-Allow-Origin','*');
     call sa_set_http_header('Content-Type','application/json');
-    select clientMail, clientPassword, clientId from tbClients
+    select clientMail, clientPassword, clientId, clientPrenom from tbClients
     where mail = clientMail AND motDePasse = clientPassword
 END;
 
