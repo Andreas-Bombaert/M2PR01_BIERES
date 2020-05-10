53adc9537cdadb13ca8f2a74105c2547da4e3d67
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 15:53:29 +0200

    Delete tbVentes.sql

--- backend/script-base-de-donnees/creations-tables/tbVentes.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "tbVentes" (
-    "commId" INTEGER NOT NULL,
-    "ligneNo" INTEGER NOT NULL,
-    "biereId" INTEGER NOT NULL,
-    "prodQuant" INTEGER NOT NULL,
-    CONSTRAINT "fk__tbVentes__tbHistoriques" FOREIGN KEY (commId) REFERENCES tbHistoriques (commId)
-);
