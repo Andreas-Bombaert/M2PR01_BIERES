a89fa9d5afe0839c46e6ab86b8c80bc333e4926e
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-05-10 16:29:27 +0200

    Create Cédric-webservice-getHistory.sql

---
+++ backend/scripts-api-rest/Cédric-webservice-getHistory.sql
@@ -0,0 +1,21 @@
+/*
+auth : Cédric
+  Procédure qui envoi le contenu de tbHistory selon le commId demandé
+  
+  input (CHAR(5)) : commId que l'on veut ('coXXX').
+  output (JSON)   : Renvoi le contenu de la tbHistory selon le code en JSON.
+*/
+
+CREATE PROCEDURE "DBA"."getHistory"(IN code CHAR(5))
+RESULT(commId CHAR(5),clientId char(5), prixTot decimal(6,2), quantTot integer, "date" datetime)
+BEGIN
+    call sa_set_http_header('Access-Control-Allow-Origin','*');
+    call sa_set_http_header('Content-Type','application/json');
+    Select commId, clientId, prixTot, quantTot, "date" from tbHistoriques
+    where commId = code
+end
+
+ ---
+/* Service pour recevoir le code JSON ( 'get'  '/history?id=co001' ).*/
+
+CREATE SERVICE "getHistory" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.getHistory(:id);
