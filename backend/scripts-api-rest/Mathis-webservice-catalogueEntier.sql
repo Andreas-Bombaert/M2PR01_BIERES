af415172c91170510cd77e86bd36dc1dd22f72a3
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 15:59:55 +0200

    Create Mathis-webservice-catalogueEntier.sql

---
+++ backend/scripts-api-rest/Mathis-webservice-catalogueEntier.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE "DBA"."proc_catalogueEntier"()
+RESULT (id char(3), biere varchar(30), couleur varchar(15), alcool decimal(3,1), volume decimal(4,2), brasserie varchar(60), prix decimal(4,2))
+BEGIN
+    call sa_set_http_header('Access-Control-Allow-Origin','*');
+    call sa_set_http_header('Content-Type','application/json');
+    select biereId, biereNom, biereCouleur, biereAlcool, biereVolume, brasseurNom, bierePrix from dba.tbBieres as biere
+    join dba.tbBrasseurs as brasseur on biere.brasseurId = brasseur.brasseurId 
+    order by biere.biereNom ASC;
+END
