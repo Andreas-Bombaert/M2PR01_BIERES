fff13eba2d101ed0e1f12d3cc90797620ed57820
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-05-10 14:44:03 +0200

    Create Cédric-webservice-insertVentes.sql

---
+++ backend/scripts-api-rest/Cédric-webservice-insertVentes.sql
@@ -0,0 +1,15 @@
+/*
+  auth: Cédric
+  
+  Permet d'ajouter dans la commande dans la table Ventes.
+  
+*/
+CREATE PROCEDURE proc_insert_tbVentes(in coId CHAR(5), in bId CHAR(3), in numL INTEGER, in qtt INTEGER)
+BEGIN
+    INSERT INTO DBA.tbVentes(commId,biereId,ligneNo,prodQuant)
+    VALUES
+    (coId,bId,numL,quant);
+END;
+
+CREATE SERVICE "insertVente" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insert_tbVentes(:coId,:bId,:numL,;qtt);
+
