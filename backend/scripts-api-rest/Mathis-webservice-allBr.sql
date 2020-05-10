43f907e156f867e779e6e62727dccc28c0648b2e
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 18:51:21 +0200

    Update Mathis-webservice-allBr.sql

--- backend/scripts-api-rest/Mathis-webservice-allBr.sql
+++ backend/scripts-api-rest/Mathis-webservice-allBr.sql
@@ -1,3 +1,8 @@
+/*
+auth : Mathis
+Creation de la procédure qui récupère toutes les bières avec des critères de tri mais accepte toutes les brasseries, 
+le tri se fait avec le prix minimum, prix maximum, degré d'alcoolémie minimum et maximum la couleur est également un critère de tri.
+*/
 CREATE PROCEDURE "DBA"."proc_catalogueAllBr"(in couleur varchar(15), in pMin decimal(4,2), in pMax decimal(4,2), in dMin decimal(3,1), in dMax decimal(3,1))
 RESULT (id char(3), biere varchar(30), couleur varchar(15), alcool decimal(3,1), volume decimal(4,2), brasserie varchar(60), prix decimal(4,2))
 BEGIN
@@ -8,5 +13,8 @@ BEGIN
     where biereCouleur = couleur AND (bierePrix BETWEEN pMin AND pMax) AND (biereAlcool BETWEEN dMin AND dMax)
     order by biere.biereNom ASC;
 END;
-
+/*
+auth : Mathis
+Creation du webservice pour la procédure catalogueAllBr
+*/
 CREATE SERVICE "allBr" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call DBA.proc_catalogueAllBr(:couleur,:pMin,:pMax,:dMin,:dMax);
