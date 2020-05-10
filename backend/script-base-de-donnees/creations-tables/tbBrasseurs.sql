8a733a8df2d3fa5448ed774815443c6787026487
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 15:53:00 +0200

    Delete tbBrasseurs.sql

--- backend/script-base-de-donnees/creations-tables/tbBrasseurs.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tbBrasseurs(
-    brasseurId CHAR(3) NOT NULL,
-    brasseurNom VARCHAR(20) NOT NULL,
-    vilId CHAR(4) NOT NULL,
-    CONSTRAINT pk__Brasseurs PRIMARY KEY (brasseurId),
-    CONSTRAINT fk__Brasseurs FOREIGN KEY (vilId) REFERENCES tbVilles (vilId)
-);
