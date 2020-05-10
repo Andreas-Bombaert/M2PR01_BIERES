06b2428446aa99c5d0a5d9d3f9580644edbe3772
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 15:53:09 +0200

    Delete tbClients.sql

--- backend/script-base-de-donnees/creations-tables/tbClients.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tbClients(
-  clientId char(5) NOT NULL,
-  clientNom VARCHAR(20) NOT NULL,
-  clientPrenom VARCHAR(20) NOT NULL,
-  clientAdresse VARCHAR(50) NOT NULL,
-  clientMail VARCHAR(50) NOT NULL,
-  CONSTRAINT pk__Clients PRIMARY KEY (clientId)
-);
