681ea3b59a44665bdb5f88596b978b4efb879496
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-05-10 16:35:56 +0200

    Update Cédric-webservice-getBiereNom.sql

--- backend/scripts-api-rest/Cédric-webservice-getBiereNom.sql
+++ backend/scripts-api-rest/Cédric-webservice-getBiereNom.sql
@@ -7,7 +7,7 @@ Retourne la liste complète des biereId et les les biereNom
 */
 
 
-CREATE PROCEDURE "DBA"."getBiereNom"()
+CREATE PROCEDURE "DBA"."proc_getBiereNom"()
 RESULT( biereId varchar(3), biereNom varchar(30))
 BEGIN
     call sa_set_http_header('Access-Control-Allow-Origin','*');
@@ -17,4 +17,4 @@ END;
 
 /* service pour la procedure */
 
-CREATE SERVICE "getBieres" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS CALL dba.getBiereNom();
+CREATE SERVICE "getBieres" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS CALL dba.proc_getBiereNom();
