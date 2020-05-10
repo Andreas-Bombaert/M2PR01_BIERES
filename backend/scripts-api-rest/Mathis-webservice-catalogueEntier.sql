620b5c7e434fd494a50306fe3abd65205ab25f68
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 18:55:32 +0200

    Update Mathis-webservice-catalogueEntier.sql

--- backend/scripts-api-rest/Mathis-webservice-catalogueEntier.sql
+++ backend/scripts-api-rest/Mathis-webservice-catalogueEntier.sql
@@ -1,3 +1,7 @@
+/*
+auth : Mathis
+Creation de la procédure qui récupère toutes les bières. Elle renvoit un id, le nom, la couleur, le degré d'alcoolémie, le volume, la brasserie et le prix.
+*/
 CREATE PROCEDURE "DBA"."proc_catalogueEntier"()
 RESULT (id char(3), biere varchar(30), couleur varchar(15), alcool decimal(3,1), volume decimal(4,2), brasserie varchar(60), prix decimal(4,2))
 BEGIN
@@ -7,5 +11,8 @@ BEGIN
     join dba.tbBrasseurs as brasseur on biere.brasseurId = brasseur.brasseurId 
     order by biere.biereNom ASC;
 END;
-
+/*
+auth : Mathis
+Creation du webservice pour la procédure catalogueEntier
+*/
 CREATE SERVICE "initBieres" TYPE 'JSON' AUTHORIZATION OFF USER "dba" URL ON METHODS 'GET' AS call dba.proc_catalogueEntier();
