39b9e36e6e291e8d13d41484283fcbfc7f40d834
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-05-10 16:31:33 +0200

    Update Cédric-webservice-insertHistorique.sql

--- backend/scripts-api-rest/Cédric-webservice-insertHistorique.sql
+++ backend/scripts-api-rest/Cédric-webservice-insertHistorique.sql
@@ -5,11 +5,10 @@ Permet d'ajouter la commande a la table Historique.
 */
 
 
-CREATE PROCEDURE proc_insert_tbHistoriques(in coId CHAR(5), in cId CHAR(5), in qttTot INTEGER)
+CREATE PROCEDURE "DBA"."proc_insert_tbHistoriques"(in coId CHAR(5), in cId CHAR(5), in prTot DECIMAL(6,2), in qttTot INTEGER)
 BEGIN
-    INSERT INTO DBA.tbHistoriques(commId,clientId,prixTot,quantTot)
-        VALUES
-    (coId,cId, calcPrixTot(coId), calcQuantTot(coId))
+    INSERT INTO DBA.tbHistoriques(prixTot,commId,clientId,quantTot)
+        VALUES (prTot,coId,cId,qttTot)
 END;
 
-CREATE SERVICE "insertHistorique" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insert_tbHistoriques(:coId,:cId);
+CREATE SERVICE "insertHistorique" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insert_tbHistoriques(:coId,:cId,:prTot,:qttTot);
