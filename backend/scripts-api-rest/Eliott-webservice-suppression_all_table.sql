9245e1a2fa00751791217eab675ba5738bdd2e1f
Eliott-Lepage <64136394+Eliott-Lepage@users.noreply.github.com>
2020-05-10 15:08:45 +0200

    Create Eliott-webservice-suppression_all_table.sql

---
+++ backend/scripts-api-rest/Eliott-webservice-suppression_all_table.sql
@@ -0,0 +1,15 @@
+/* auth: Eliott Lepage
+
+Création de de la procédure qui supprime la totalité de la table tbPanier contenant les bières.
+
+*/
+
+CREATE PROCEDURE "DBA"."proc_suppression_all_table"()
+BEGIN 
+    DELETE FROM tbPanier;
+END;
+
+
+/* Création du webservice lié à la procédure */
+
+CREATE SERVICE "suppression_all" TYPE 'RAW' AUTHORIZATION OFF USER "DBA" URL ON METHODS 'GET' AS call DBA.proc_suppression_all_table();
