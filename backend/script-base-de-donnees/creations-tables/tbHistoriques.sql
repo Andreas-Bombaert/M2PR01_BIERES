1000e21bee198b510eb05157b71b8596276231ec
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 15:53:16 +0200

    Delete tbHistoriques.sql

--- backend/script-base-de-donnees/creations-tables/tbHistoriques.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "tbHistoriques" (
-    "commId" INTEGER NOT NULL DEFAULT AUTOINCREMENT,
-    "clientId" INTEGER NOT NULL,
-    "prixTot" decimal(6,2) NOT NULL,
-    "quantTot" INTEGER NOT NULL,
-    "date" "datetime" NOT NULL DEFAULT getDate(),
-    CONSTRAINT "pk__tbHistoriques" PRIMARY KEY ( "commId" ASC )
-);
