6e83f57c86a517db5835d7381337d24d8563bdcc
Mathis <64088463+Mathis-Dory@users.noreply.github.com>
2020-05-10 15:58:15 +0200

    Create Mathis-webservice-allBrAllColors.sql

---
+++ backend/scripts-api-rest/Mathis-webservice-allBrAllColors.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE "DBA"."proc_catalogueAllBrAllColors"(in pMin decimal(4,2), in pMax decimal(4,2), in dMin decimal(3,1), in dMax decimal(3,1))
+RESULT (id char(3), biere varchar(30), couleur varchar(15), alcool decimal(3,1), volume decimal(4,2), brasserie varchar(60), prix decimal(4,2))
+BEGIN
+    call sa_set_http_header('Access-Control-Allow-Origin','*');
+    call sa_set_http_header('Content-Type','application/json');
+    select biereId, biereNom, biereCouleur, biereAlcool, biereVolume, brasseurNom, bierePrix from dba.tbBieres as biere
+    join dba.tbBrasseurs as brasseur on biere.brasseurId = brasseur.brasseurId 
+    where (bierePrix BETWEEN pMin AND pMax) AND (biereAlcool BETWEEN dMin AND dMax)
+    order by biere.biereNom ASC;
+END
