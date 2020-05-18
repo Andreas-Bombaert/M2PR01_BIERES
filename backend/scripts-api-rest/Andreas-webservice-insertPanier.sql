ff1f5b85fad8394ffbb39ce86dc06f1486352051
Cédric De Dryver <64083179+DeDryverC@users.noreply.github.com>
2020-05-18 11:25:00 +0200

    Update Andreas-webservice-insertPanier.sql

--- backend/scripts-api-rest/Andreas-webservice-insertPanier.sql
+++ backend/scripts-api-rest/Andreas-webservice-insertPanier.sql
@@ -4,11 +4,16 @@ modifier en fonction de comment on appelera la table panier
 */
 
 CREATE PROCEDURE "DBA"."proc_insert_panier"(in bId char(3), in qtt integer, in cId char(5))
+Result(quant INTEGER)
 BEGIN 
     INSERT INTO dba.tbPanier (clientId,biereId,quantite)
     VALUES 
     (cId,bId,qtt);
-END;
+
+    call sa_set_http_header('Access-Control-Allow-Origin','*');
+    call sa_set_http_header('Content-Type','application/json');
+    select calcProdPanier(cId,bId);
+END
 
 
-CREATE SERVICE "insertPanier" TYPE 'RAW' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insert_panier(:bId,:qtt,:cId);
+CREATE SERVICE "insertPanier" TYPE 'JSON' AUTHORIZATION OFF USER "dba" METHODS 'GET' AS call dba.proc_insert_panier(:bId,:qtt,:cId);
